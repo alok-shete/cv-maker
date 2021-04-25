955818a8736ec50ce823456d2d39cb2ebc4dc43c
Alok shete <53371864+alok-shete@users.noreply.github.com>
2021-04-25 13:33:09 +0530

    Update config.php

--- include/config.php
+++ include/config.php
@@ -2,4 +2,13 @@
     $client = new MongoDB\Client(getenv("DATABASE_URL"));
     $db           = $client->cv;
     $collection   = $db->data;
+    $ip_server = $_SERVER['SERVER_ADDR'];
+    debug_to_console($ip_server);
+    function debug_to_console($data) {
+        $output = $data;
+        if (is_array($output))
+            $output = implode(',', $output);
+
+        echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+    }
 ?>
